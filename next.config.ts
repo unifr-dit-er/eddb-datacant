d6f1724a8ed5b33c9701339df08bcb8057d2593d
alrick <alrick.deillon@unifr.ch>
2026-06-09 11:41:46 +0200

    feat: update basePath to /datacant in configuration and environment files

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  basePath: '/dataprotectionlaw',
+  basePath: '/datacant',
   images: { unoptimized: true },
 };
 
